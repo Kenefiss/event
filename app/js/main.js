50e8f3a364b8fb39ade77014699b0bd5953ad0ce
Kenefiss <kenefiss@gmail.com>
2020-03-31 17:31:08 +0300

    bugs fix-2

--- app/js/main.js
+++ app/js/main.js
@@ -44,7 +44,7 @@ $(function () {
 
   // ===== Scroll to Top ==== 
   $(window).scroll(function () {
-    if ($(this).scrollTop() >= 200) { // If page is scrolled more than 50px
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
       $('#return-to-top').fadeIn(400); // Fade in the arrow
     } else {
       $('#return-to-top').fadeOut(400); // Else fade out the arrow
